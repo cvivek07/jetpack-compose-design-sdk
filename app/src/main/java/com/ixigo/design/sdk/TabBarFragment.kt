47da22fe540ee69286d1b551f979f7ed7de3208c
Vivek Singh <vivek.singh@travenues.com>
2022-12-19 16:03:49 +0530

    Tabs type added in fragments

--- app/src/main/java/com/ixigo/design/sdk/TabBarFragment.kt
+++ app/src/main/java/com/ixigo/design/sdk/TabBarFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayout.MODE_AUTO
+import com.google.android.material.tabs.TabLayout.MODE_FIXED
 import com.ixigo.design.sdk.components.tabs.base.TabType
 import com.ixigo.design.sdk.components.topappbar.TabItem
 import com.ixigo.design.sdk.databinding.FragmentButtonsBinding
@@ -32,10 +34,19 @@ class TabBarFragment: Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val fragments = listOf(TypographyFragment(), ButtonsFragment())
         val dataList = listOf(TabItem("Typography", 0,0), TabItem("Buttons", 0,0))
-        binding.viewPager.adapter = PagerAdapter(parentFragmentManager, lifecycle, fragments)
 
+        binding.viewPager.adapter = PagerAdapter(parentFragmentManager, lifecycle, fragments)
         binding.tabLayout.tabType = TabType.LINED
+        binding.tabLayout.tabMode =  MODE_FIXED
         binding.tabLayout.setupWithViewPager2(binding.viewPager, dataList)
+
+        val fragments2 = listOf(ProgressStepFragment(), InputFieldsFragment())
+        val dataList2 = listOf(TabItem("Progress Step", 0,0), TabItem("Input Field", 0,0))
+
+        binding.viewPager2.adapter = PagerAdapter(parentFragmentManager, lifecycle, fragments2)
+        binding.linedTabLayout.tabType = TabType.PILL
+        binding.tabLayout.tabMode =  MODE_AUTO
+        binding.linedTabLayout.setupWithViewPager2(binding.viewPager2, dataList2)
     }
 
     class PagerAdapter(
