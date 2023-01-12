f8a522c0c7ad29b27f3e042237e832969a8154db
sankalp.singh <sankalp.singh@travenues.com>
2023-01-13 03:23:04 +0530

    feat: chip

---
+++ ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/chip/IxiPrimaryChip.kt
@@ -0,0 +1,52 @@
+package com.ixigo.design.sdk.components.chip
+
+import android.content.Context
+import android.util.AttributeSet
+import com.ixigo.design.sdk.R
+import com.ixigo.design.sdk.components.chip.base.BaseChip
+import com.ixigo.design.sdk.components.styles.IxiChipColor
+
+
+class IxiPrimaryChip @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : BaseChip(context, attrs, defStyleAttr)  {
+    init {
+        val typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.IxiPrimaryChip)
+        try {
+            val backgroundColorVal: Int =
+                typedArray.getResourceId(R.styleable.IxiPrimaryChip_android_background, -1)
+            val backgroundColor = if (backgroundColorVal != -1) backgroundColorVal else android.R.color.transparent
+            val drawableTintColorVal: Int =
+                typedArray.getResourceId(R.styleable.IxiPrimaryChip_android_drawableTint, -1)
+            val drawableTintColor = if (drawableTintColorVal != -1) drawableTintColorVal else android.R.color.transparent
+            val textColorVal: Int = typedArray.getResourceId(R.styleable.IxiPrimaryChip_android_textColor, -1)
+            val textColor = if (textColorVal != -1) textColorVal else R.color.black
+            isEnabled(typedArray.getBoolean(R.styleable.IxiPrimaryChip_android_enabled, true))
+            setIxiChipColor(IxiChipColor(backgroundColor = backgroundColor, textColor = textColor, drawableTintColor = drawableTintColor))
+        } finally {
+            typedArray.recycle()
+        }
+    }
+
+    override fun isEnabled(boolean: Boolean) {
+        if(!boolean) {
+            setIxiChipColor(null)
+        } else{
+            setIxiChipColor(color)
+        }
+        super.isEnabled(boolean)
+    }
+
+    public override fun setIxiChipColor(color: IxiChipColor?) {
+        if(color!=null) {
+            this.color = color
+           super.setIxiChipColor(IxiChipColor(
+               backgroundColor = color.backgroundColor,
+               textColor = color.textColor,
+               drawableTintColor = color.drawableTintColor,
+           ))
+        } else{
+            super.setIxiChipColor(IxiChipColor.PrimaryDisabled)
+        }
+    }
+}
--- ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/styles/IxiColor.kt
+++ ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/styles/IxiColor.kt
@@ -48,3 +48,144 @@ sealed class IxiColor(
     internal object ExtensionTertiary :
         IxiColor(android.R.color.transparent, android.R.color.transparent, R.color.p500)
 }
+
+open class IxiChipColor(
+    @ColorRes val backgroundColor: Int = android.R.color.transparent,
+    @ColorRes val strokeColor: Int? = null,
+    @ColorRes val textColor: Int = R.color.n800,
+    @ColorRes val drawableTintColor: Int? = null,
+
+){
+    object PrimaryDisabled : IxiChipColor(
+        backgroundColor = R.color.n40,
+        textColor = R.color.n300,
+        drawableTintColor = R.color.n300
+    )
+
+    object SecondaryDisabled : IxiChipColor(
+        backgroundColor = R.color.n20,
+        strokeColor = R.color.n40,
+        textColor = R.color.n300,
+        drawableTintColor = R.color.n300
+    )
+
+    object OutlinedDisabled : IxiChipColor(
+        backgroundColor = android.R.color.transparent,
+        strokeColor = R.color.n40,
+        textColor = R.color.n300,
+        drawableTintColor = R.color.n300
+    )
+
+    object TextDisabled : IxiChipColor(
+        backgroundColor = android.R.color.transparent,
+        textColor = R.color.n300,
+        drawableTintColor = R.color.n300
+    )
+    object Primary{
+        object NEUTRAL{
+            val background900 = IxiChipColor(backgroundColor = R.color.n900, textColor = R.color.white)
+            val background800 = IxiChipColor(backgroundColor = R.color.n800, textColor = R.color.white)
+        }
+        object BLUE{
+            val background500 = IxiChipColor(backgroundColor = R.color.b500, textColor = R.color.white)
+            val background400 = IxiChipColor(backgroundColor = R.color.b400, textColor = R.color.white)
+        }
+        object GREEN{
+            val background500 = IxiChipColor(backgroundColor = R.color.g500, textColor = R.color.white)
+            val background400 = IxiChipColor(backgroundColor = R.color.g400, textColor = R.color.white)
+        }
+        object RED{
+            val background500 = IxiChipColor(backgroundColor = R.color.r500, textColor = R.color.white)
+            val background400 = IxiChipColor(backgroundColor = R.color.r400, textColor = R.color.white)
+        }
+        object YELLOW{
+            val background500 = IxiChipColor(backgroundColor = R.color.y500, textColor = R.color.white)
+            val background400 = IxiChipColor(backgroundColor = R.color.y400, textColor = R.color.white)
+        }
+        object PURPLE{
+            val background500 = IxiChipColor(backgroundColor = R.color.p500, textColor = R.color.white)
+            val background400 = IxiChipColor(backgroundColor = R.color.p400, textColor = R.color.white)
+        }
+    }
+
+    object Secondary{
+        object NEUTRAL{
+            val background40 = IxiChipColor(backgroundColor = R.color.n40, strokeColor = R.color.n100,  textColor = R.color.n800)
+            val background80= IxiChipColor(backgroundColor = R.color.n80, strokeColor = R.color.n100, textColor = R.color.n800)
+        }
+        object BLUE{
+            val background50 = IxiChipColor(backgroundColor = R.color.b50, strokeColor = R.color.b100,  textColor = R.color.b500)
+            val background100 = IxiChipColor(backgroundColor = R.color.b100, strokeColor = R.color.b100, textColor = R.color.b500)
+        }
+        object GREEN{
+            val background50 = IxiChipColor(backgroundColor = R.color.g50, strokeColor = R.color.g100,  textColor = R.color.g500)
+            val background100 = IxiChipColor(backgroundColor = R.color.g100, strokeColor = R.color.g100, textColor = R.color.g500)
+        }
+        object RED{
+            val background50 = IxiChipColor(backgroundColor = R.color.r50, strokeColor = R.color.r100,  textColor = R.color.r500)
+            val background100 = IxiChipColor(backgroundColor = R.color.r100, strokeColor = R.color.r100, textColor = R.color.r500)
+        }
+        object YELLOW{
+            val background50 = IxiChipColor(backgroundColor = R.color.y50, strokeColor = R.color.y100,  textColor = R.color.y500)
+            val background100 = IxiChipColor(backgroundColor = R.color.y100, strokeColor = R.color.y100, textColor = R.color.y500)
+        }
+        object PURPLE{
+            val background50 = IxiChipColor(backgroundColor = R.color.p50, strokeColor = R.color.p100,  textColor = R.color.p500)
+            val background100 = IxiChipColor(backgroundColor = R.color.p100, strokeColor = R.color.p100, textColor = R.color.p500)
+        }
+    }
+
+    object Outlined{
+        object NEUTRAL{
+            val stroke300 = IxiChipColor(strokeColor = R.color.n300,  textColor = R.color.n800)
+            val background40Stroke300 = IxiChipColor(backgroundColor = R.color.n40, strokeColor = R.color.n300, textColor = R.color.n800)
+        }
+        object BLUE{
+            val stroke300 = IxiChipColor(strokeColor = R.color.b300,  textColor = R.color.b500)
+            val background50Stroke300 = IxiChipColor(backgroundColor = R.color.b50, strokeColor = R.color.b300, textColor = R.color.b500)
+        }
+        object GREEN{
+            val stroke300 = IxiChipColor(strokeColor = R.color.g300,  textColor = R.color.g500)
+            val background50Stroke300 = IxiChipColor(backgroundColor = R.color.g50, strokeColor = R.color.g300, textColor = R.color.g500)
+        }
+        object RED{
+            val stroke300 = IxiChipColor(strokeColor = R.color.r300,  textColor = R.color.r500)
+            val background50Stroke300 = IxiChipColor(backgroundColor = R.color.r50, strokeColor = R.color.r300, textColor = R.color.r500)
+        }
+        object YELLOW{
+            val stroke300 = IxiChipColor(strokeColor = R.color.y300,  textColor = R.color.y500)
+            val background50Stroke300 = IxiChipColor(backgroundColor = R.color.y50, strokeColor = R.color.y300, textColor = R.color.y500)
+        }
+        object PURPLE{
+            val stroke300 = IxiChipColor(strokeColor = R.color.p300,  textColor = R.color.p500)
+            val background50Stroke300 = IxiChipColor(backgroundColor = R.color.p50, strokeColor = R.color.p300, textColor = R.color.p500)
+        }
+    }
+
+    object Text{
+        object NEUTRAL{
+            val text800 = IxiChipColor(textColor = R.color.n800)
+            val background40Text800 = IxiChipColor(backgroundColor = R.color.n40, textColor = R.color.n800)
+        }
+        object BLUE{
+            val text500 = IxiChipColor(textColor = R.color.b500)
+            val background50Text500 = IxiChipColor(backgroundColor = R.color.b50, textColor = R.color.b500)
+        }
+        object GREEN{
+            val text500 = IxiChipColor(textColor = R.color.g500)
+            val background50Text500 = IxiChipColor(backgroundColor = R.color.g50, textColor = R.color.g500)
+        }
+        object RED{
+            val text500 = IxiChipColor(textColor = R.color.r500)
+            val background50Text500 = IxiChipColor(backgroundColor = R.color.r50, textColor = R.color.r500)
+        }
+        object YELLOW{
+            val text500 = IxiChipColor(textColor = R.color.y500)
+            val background50Text500 = IxiChipColor(backgroundColor = R.color.y50, textColor = R.color.y500)
+        }
+        object PURPLE{
+            val text500 = IxiChipColor(textColor = R.color.p500)
+            val background50Text500 = IxiChipColor(backgroundColor = R.color.p50, textColor = R.color.p500)
+        }
+    }
+}
