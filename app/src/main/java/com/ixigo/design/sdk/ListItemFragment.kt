3d90b10cab0d292cd6c8056b85c784e5893c8d63
Vivek Singh <vivek.singh@travenues.com>
2023-02-03 11:33:19 +0530

    Merge conflicts resolved

--- app/src/main/java/com/ixigo/design/sdk/ListItemFragment.kt
+++ app/src/main/java/com/ixigo/design/sdk/ListItemFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.setPadding
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
