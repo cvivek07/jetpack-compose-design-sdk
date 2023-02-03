8f3a57e5158bc6c32ec899d02070ae16ba7b7eec
sankalp.singh <sankalp.singh@travenues.com>
2023-02-03 14:33:54 +0530

    fix: Icon bottomsheet

--- app/src/main/java/com/ixigo/design/sdk/BottomSheetFragment.kt
+++ app/src/main/java/com/ixigo/design/sdk/BottomSheetFragment.kt
@@ -65,7 +65,7 @@ class BottomSheetFragment: Fragment() {
 
         //variant 3
         binding.third.setClickListener {
-            IxiBottomSheetHelper().showFeatureIconBottomSheet(
+            IxiBottomSheetHelper().showNoIconBottomSheet(
                 IxiBottomSheetHelper.IxiBottomSheetUiModel(
                     titleText = "Main title sentence",
                     bodyText = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
