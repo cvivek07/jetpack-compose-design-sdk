67b17f039a0a276d5277057ecee2527f41717f5d
Vivek Singh <vivek.singh@travenues.com>
2023-02-26 16:59:21 +0530

    fragment manager refactored out of BottomSheet

--- ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/bottomsheets/helper/IxiBottomSheetHelper.kt
+++ ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/bottomsheets/helper/IxiBottomSheetHelper.kt
@@ -3,7 +3,6 @@ package com.ixigo.design.sdk.components.bottomsheets.helper
 import android.view.View
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
-import androidx.fragment.app.FragmentManager
 import com.ixigo.design.sdk.components.bottomsheets.IxiBottomSheetDialogFragment
 import com.ixigo.design.sdk.components.bottomsheets.IxiBottomSheetView
 import com.ixigo.design.sdk.components.styles.IxiColor
@@ -33,7 +32,7 @@ object IxiBottomSheetHelper {
         return bottomSheet
     }
 
-    fun showImageBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel, fragmentManager: FragmentManager){
+    fun getImageBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel): IxiBottomSheetDialogFragment{
         val fragment = addBasePropertiesToBottomSheet(ixiBottomSheetUiModel, IxiBottomSheetDialogFragment.newInstance())
         fragment.setImageBackgroundColor(ixiBottomSheetUiModel.imageBackgroundColor)
         ixiBottomSheetUiModel.iconSize?.let {
@@ -43,30 +42,32 @@ object IxiBottomSheetHelper {
         ixiBottomSheetUiModel.inlineAlertText?.let {
             fragment.setInlineAlert(it, ixiBottomSheetUiModel.inlineAlertIxiColor)
         }
-        fragment.show(fragmentManager, IxiBottomSheetDialogFragment.TAG)
+        return fragment
     }
 
 
-    fun showFeatureIconBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel, fragmentManager: FragmentManager){
+    fun getFeatureIconBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel):IxiBottomSheetDialogFragment{
         val fragment = addBasePropertiesToBottomSheet(ixiBottomSheetUiModel, IxiBottomSheetDialogFragment.newInstance())
         ixiBottomSheetUiModel.iconSize?.let {
             fragment.setIconSize(it)
         }
         fragment.setImage(ixiBottomSheetUiModel.image)
-        fragment.show(fragmentManager, IxiBottomSheetDialogFragment.TAG)
+        return fragment
     }
 
-    fun showNoIconBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel, fragmentManager: FragmentManager){
-        val fragment = addBasePropertiesToBottomSheet(ixiBottomSheetUiModel, IxiBottomSheetDialogFragment.newInstance())
-        fragment.show(fragmentManager, IxiBottomSheetDialogFragment.TAG)
+    fun getNoIconBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel): IxiBottomSheetDialogFragment {
+        return addBasePropertiesToBottomSheet(
+            ixiBottomSheetUiModel,
+            IxiBottomSheetDialogFragment.newInstance()
+        )
     }
 
-    fun showBlankBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel, fragmentManager: FragmentManager){
+    fun getBlankBottomSheet(ixiBottomSheetUiModel: IxiBottomSheetUiModel): IxiBottomSheetDialogFragment{
         val fragment = addBasePropertiesToBottomSheet(ixiBottomSheetUiModel, IxiBottomSheetDialogFragment.newInstance())
         ixiBottomSheetUiModel.view?.let {
             fragment.setView(ixiBottomSheetUiModel.view)
         }
-        fragment.show(fragmentManager, IxiBottomSheetDialogFragment.TAG)
+        return fragment
     }
 
     data class IxiBottomSheetUiModel(
