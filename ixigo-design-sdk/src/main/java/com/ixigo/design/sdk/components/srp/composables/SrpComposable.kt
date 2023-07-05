e33c5193fa166f702a8eab24fd69f24e9b5c723e
vivek.choudhary@travenues.com <>
2023-07-05 16:32:11 +0530

    fix: srp toolbar alignment issues

--- ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/srp/composables/SrpComposable.kt
+++ ixigo-design-sdk/src/main/java/com/ixigo/design/sdk/components/srp/composables/SrpComposable.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ixigo.design.sdk.R
@@ -41,8 +42,7 @@ fun SrpComposable(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row( modifier = Modifier.weight(1f),
-            verticalAlignment = Alignment.CenterVertically,
+        Row(verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Center
         ) {
             TypographyText(text = title.startText, textStyle = IxiTypography.Body.Small.medium)
@@ -58,7 +58,8 @@ fun SrpComposable(
             TypographyText(
                 text = subTitle,
                 textStyle = IxiTypography.Body.XSmall.regular,
-                modifier = Modifier.align(Alignment.CenterHorizontally).weight(1f),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.wrapContentHeight().padding(top = 2.dp),
                 color = colorResource(id = R.color.n600),
             )
         }
