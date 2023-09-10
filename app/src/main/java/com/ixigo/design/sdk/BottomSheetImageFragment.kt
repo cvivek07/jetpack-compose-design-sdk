f7f12d3c54a518bfbf19184e70b4e09f4b5d6d70
vivek.choudhary@travenues.com <>
2023-09-10 17:48:14 +0530

    feat: bottom sheet api changes, added composable content in bottom sheet state

--- app/src/main/java/com/ixigo/design/sdk/BottomSheetImageFragment.kt
+++ app/src/main/java/com/ixigo/design/sdk/BottomSheetImageFragment.kt
@@ -1,45 +1,29 @@
 package com.ixigo.design.sdk
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.ixigo.design.sdk.components.bottomsheets.IxiBottomSheetDialogFragment
-import com.ixigo.design.sdk.components.bottomsheets.base.BottomSheetState
 
 class BottomSheetImageFragment : IxiBottomSheetDialogFragment() {
 
-    override fun createBottomSheetView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        state: BottomSheetState
-    ): View {
-        val state = BottomSheetState(
-            headerText = "Main title sentence",
-            bodyText = "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-            imageBackgroundColor = R.color.r50,
-            iconSize = 127f,
-            image = R.drawable.sample_image,
-            primaryButtonText = "Button",
-            primaryActionListener = {
-                dismiss()
-                "Primary Button".toToast(requireContext())
-            },
-            secondaryButtonText = "Button",
-            secondaryActionListener = {
-                dismiss()
-                "Secondary Button".toToast(requireContext())
-            },
-            inlineAlertText = "This is a placeholder",
-            secondaryButtonHelperText = "Hello",
-            primaryButtonHelperText = "Hey!"
-        )
-        return super.createBottomSheetView(inflater, container, state)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // perform operations
+        setHeaderText("Main title sentence")
+        setBodyText("Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
+        setImageBackgroundColor(R.color.r50)
+        setIconSize(127f)
+        setImage(R.drawable.sample_image)
+        setPrimaryButton("Button", "Hey!")
+        setPrimaryButtonActionListener {
+            dismiss()
+            "Primary Button".toToast(requireContext())
+        }
+        setSecondaryButton("Button", "Hello")
+        setSecondaryButtonActionListener{
+            dismiss()
+            "Secondary Button".toToast(requireContext())
+        }
+        setInlineAlert("This is a placeholder")
     }
 
     companion object {
--- app/src/main/java/com/ixigo/design/sdk/BottomSheetViewFragment.kt
+++ app/src/main/java/com/ixigo/design/sdk/BottomSheetViewFragment.kt
@@ -6,49 +6,34 @@ import android.view.View
 import android.view.ViewGroup
 import com.ixigo.design.sdk.components.bottomsheets.IxiBottomSheetDialogFragment
 import com.ixigo.design.sdk.components.bottomsheets.IxiBottomSheetView
-import com.ixigo.design.sdk.components.bottomsheets.base.BottomSheetState
-import com.ixigo.design.sdk.databinding.FragmentBottomSheetBinding
+import com.ixigo.design.sdk.databinding.FragmentBlankBottomSheetBinding
 
 class BottomSheetViewFragment : IxiBottomSheetDialogFragment() {
 
-    private lateinit var binding: FragmentBottomSheetBinding
+    private lateinit var binding: FragmentBlankBottomSheetBinding
 
-    override fun createBottomSheetView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        state: BottomSheetState
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        binding = FragmentBottomSheetBinding.inflate(
-            inflater,
-            container,
-            false
-        )
-        val state = BottomSheetState(
-            toolbarText = "Toolbar Title",
-            closeActionAlignment = IxiBottomSheetView.ActionIconAlignment.END,
-            disableDragging = true,
-            primaryButtonText = "Yes",
-            primaryActionListener = {
-                dismiss()
-                "Primary Action".toToast(requireContext())
-            },
-            secondaryButtonText = "No",
-            secondaryActionListener = {
-                dismiss()
-                "Secondary action".toToast(requireContext())
-            },
-            onClose = {
-                dismiss()
-                "Close Action".toToast(requireContext())
-            },
-            view = binding.root
-        )
-        return super.createBottomSheetView(inflater, container, state)
+        binding = FragmentBlankBottomSheetBinding.inflate(inflater, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // perform operations
+        setToolbarTitle("Toolbar Title")
+        setCloseActionAlignment(IxiBottomSheetView.ActionIconAlignment.START)
+        disableDragging(true)
+        setPrimaryButton("Yes", "helper text")
+        setPrimaryButtonActionListener { dismiss() }
+        setSecondaryButton("No", "helper")
+        setSecondaryButtonActionListener {
+            dismiss()
+        }
+        setCloseActionListener {
+            dismiss()
+        }
+        setView(binding.root)
     }
 
     companion object {
